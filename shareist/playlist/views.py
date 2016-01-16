ae6a06aca00de1bc26f657c9bc75253eb25a4b3e
anurag krishna <anurag.bits18@gmail.com>
2016-01-16 22:15:42 +0530

    Added Permission class to /track.
    Signed-off-by: anurag krishna <anurag.bits18@gmail.com>

--- shareist/playlist/views.py
+++ shareist/playlist/views.py
@@ -71,21 +71,23 @@ class Playlist_Detail(APIView):
 #/track
 class Track_List(APIView):
 
-    def get(self, request, format=None):
-        tracks = Track.objects.all()
-        serializer = TrackSerializer(tracks, many=True)
-        return Response(serializer.data)
+	permission_classes = (UserPermissions,)
+	def get(self, request, format=None):
+		tracks = Track.objects.all()
+		serializer = TrackSerializer(tracks, many=True)
+		return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = PlaylistSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	def post(self, request, format=None):
+		serializer = PlaylistSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 #track/<pk>
 class Track_Detail(APIView):
 
+	permission_classes = (UserPermissions,)
 	def get_object(self, pk):
 		try:
 			return Track.objects.get(pk=pk)
