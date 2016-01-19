a161abca9cf0bcf362be622b03b101c236d18935
anurag krishna <anurag.bits18@gmail.com>
2016-01-19 09:55:37 +0530

    Using ModelViewSet instead of generics (so simple!!)
    Signed-off-by: anurag krishna <anurag.bits18@gmail.com>

--- shareist/playlist/urls.py
+++ shareist/playlist/urls.py
@@ -18,16 +18,51 @@ from django.contrib import admin
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+playlist_list = views.PlaylistViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+playlist_detail = views.PlaylistViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+track_list = views.TrackViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+track_detail = views.TrackViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+user_list = views.UserViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+user_detail = views.UserViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
 urlpatterns = [
+    url(r'^$', views.api_root),
     url(r'^api/$', views.api_root),
-    url(r'^api/playlist/$',views.Playlist_List.as_view(), name='playlist-list'),
-    url(r'^api/playlist/(?P<pk>[0-9]+)/$', views.Playlist_Detail.as_view(),name='playlist-detail'),
 
-    url(r'^api/track/$',views.Track_List.as_view(), name='track-list'),
-    url(r'^api/track/(?P<pk>[0-9]+)/$', views.Track_Detail.as_view(),name='track-detail'),
+    url(r'^api/playlist/$',playlist_list, name='playlist-list'),
+    url(r'^api/playlist/(?P<pk>[0-9]+)/$', playlist_detail,name='playlist-detail'),
 
-    url(r'^api/users/$', views.UserList.as_view(), name='user-list'),
-	url(r'^api/users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+    url(r'^api/track/$',track_list, name='track-list'),
+    url(r'^api/track/(?P<pk>[0-9]+)/$', track_detail,name='track-detail'),
+
+    url(r'^api/users/$', user_list, name='user-list'),
+	url(r'^api/users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
 
 ]
 
--- shareist/playlist/views.py
+++ shareist/playlist/views.py
@@ -1,18 +1,15 @@
 from playlist.models import Playlist, Track
 from playlist.serializers import PlaylistSerializer, TrackSerializer, UserSerializer
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework import permissions
-from rest_framework import generics
-from shareist.permissions import IsPlaylistOwner
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
 
+
+#/api/
 @api_view(('GET',))
 def api_root(request, format=None):
     return Response({
@@ -20,106 +17,33 @@ def api_root(request, format=None):
         'tracks': reverse('track-list', request=request, format=format)
     })
 
-class UserList(generics.ListAPIView):
+
+#api/user
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+#/api/playlist
+class PlaylistViewSet(viewsets.ModelViewSet):
+    serializer_class = PlaylistSerializer
+    permission_classes=[permissions.IsAuthenticated, ]
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def get_queryset(self):
+        return self.request.user.playlists.all()
 
+    def perform_create(self, serializer):
+        serializer.data.owner = self.request.user
+        super(PlaylistViewSet, self).perform_create(serializer)
 
-#/playlist
-class Playlist_List(APIView):
+#/api/track
+class TrackViewSet(viewsets.ModelViewSet):
+    serializer_class = TrackSerializer
+    permission_classes=[permissions.IsAuthenticated, ]
 
-	permission_classes=(permissions.IsAuthenticated, IsPlaylistOwner)
-
-	def get(self, request, format=None):
-		playlists = Playlist.objects.all().filter(owner=request.user)
-		serializer = PlaylistSerializer(playlists, many=True, context={'request': request})
-		return Response(serializer.data)
-
-	def post(self, request, format=None):
-		serializer = PlaylistSerializer(data=request.data, context={'request': request})
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	def perform_create(self, serializer):
-		serializer.save(owner=self.request.user)	
-
-#/playlist/<pk>
-class Playlist_Detail(APIView):
-
-    permission_classes = (permissions.IsAuthenticated,IsPlaylistOwner)
-
-    def get_object(self, pk, request):
-    	try:
-    		return Playlist.objects.get(pk=pk, owner=request.user)
-    	except Playlist.DoesNotExist:
-    		raise Http404
-
-    def get(self, request, pk, format=None):
-    	playlist = self.get_object(pk, request)
-    	serializer = PlaylistSerializer(playlist, context={'request': request})
-    	return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-    	playlist = self.get_object(pk)
-    	serializer = PlaylistSerializer(playlist, data=request.data, context={'request': request})
-    	if serializer.is_valid():
-    		serializer.save()
-    		return Response(serializer.data)
-    	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-    	playlist = self.get_object(pk)
-    	playlist.delete()
-    	return Response(status=status.HTTP_204_NO_CONTENT)        
-
-#/track
-class Track_List(APIView):
-
-	permission_classes=(permissions.IsAuthenticated, IsPlaylistOwner)
-
-	def get(self, request, format=None):
-		tracks = Track.objects.all().filter(owner=request.user)
-		serializer = TrackSerializer(tracks, many=True, context={'request': request})
-		return Response(serializer.data)
-
-	def post(self, request, format=None):
-		serializer = TrackSerializer(data=request.data, context={'request': request})
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#track/<pk>
-class Track_Detail(APIView):
-
-	permission_classes=(permissions.IsAuthenticated, IsPlaylistOwner)
-	def get_object(self, pk, request):
-		try:
-			return Track.objects.get(pk=pk, owner=request.user)
-		except Track.DoesNotExist:
-			raise Http404
-
-	def get(self, request, pk, format=None):
-		track = self.get_object(pk, request)
-		serializer = TrackSerializer(track, context={'request': request})
-		return Response(serializer.data)
-
-	def put(self, request, pk, format=None):
-		track = self.get_object(pk)
-		serializer = TrackSerializer(track, data=request.data, context={'request': request})
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	def delete(self, request, pk, format=None):
-		track = self.get_object(pk)
-		track.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)            
+    def get_queryset(self):
+        return self.request.user.tracks.all()
+        
+    def perform_create(self, serializer):
+        serializer.data.owner = self.request.user
+        super(TrackViewSet, self).perform_create(serializer)        
+              
